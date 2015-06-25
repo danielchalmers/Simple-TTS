fb61d0b6d7a276f7772c52650b75e02ef6cbb597
Daniel Chalmers <moondpc@gmail.com>
2015-06-24 20:43:51 -0400

    Change "Speed" option default value

--- SimpleTTSReader/Properties/Settings.Designer.cs
+++ SimpleTTSReader/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace SimpleTTSReader.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("12")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10")]
         public int Speed {
             get {
                 return ((int)(this["Speed"]));
